80ed05fc422313abe2bfae89679b32365e14c687
Mahmoud Adel <capoking3@gmail.com>
2025-09-01 15:53:40 +0300

    Refactor authentication and email verification logic
    - Update controllers for password confirmation, email verification, and registration
    - Modify User model for improved authentication flow
    - Update web routes for auth features
    - Refactor and enhance authentication, registration, and email verification tests

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 // Admin Routes
-Route::prefix('admin')->middleware(['auth',IsAdmin::class])->name('admin.')->group(function () {
+Route::prefix('admin')->middleware(['auth','verified',IsAdmin::class])->name('admin.')->group(function () {
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -63,7 +63,7 @@ Route::prefix('admin')->middleware(['auth',IsAdmin::class])->name('admin.')->gro
 
 
 //User Routes
-Route::prefix('User')->group(function () {
+Route::prefix('User')->middleware(['auth','verified'])->group(function () {
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('user.dashboard');
     Route::get('/movies', [UserController::class, 'movies'])->name('user.movies');
     Route::get('/showtimes', [UserController::class, 'showtimes'])->name('user.showtimes');
